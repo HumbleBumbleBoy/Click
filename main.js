41e7c451a1a6df4432324433d0692321ebba1b7c
Humble Bumble <s.czarnowski.2008@wp.pl>
2025-10-03 12:31:33 +0200

    implemented fetching and storing of the database

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const pointsText = document.getElementById("points");
 const CPSText = document.getElementById("CPSText");
 const CPCText =  document.getElementById("CPCText");
-let gameData;
 
-let points = 1000;
+let originalData;
+let dynamicData = {};
+let points = 1000;  // debug purposes
 pointsText.innerHTML = points;
 let pointsToAdd = 1;
 let pointsPerSecond = 0;
@@ -12,7 +13,15 @@ updateStats();
 fetch("database.json")
     .then(data => data.json())
     .then(jsonData => {
-        gameData = jsonData;
+        originalData = jsonData;
+
+        originalData.infinite_upgrades.forEach(upgrade => {
+            dynamicData[upgrade.upgrade_id] = {
+                upgradeId: upgrade.upgrade_id,
+                upgradeQuantity: upgrade.upgrade_quantity,
+                upgradeCost: upgrade.upgrade_cost
+        }
+        });
     })
     .catch(error => {
     console.error('Error loading upgrades:', error);
@@ -33,3 +42,18 @@ function updateStats() {
     CPSText.innerHTML = `CPS: <span>${pointsPerSecond}</span>`;
     pointsText.innerHTML = points;
 }
+
+/*
+const testInsert = document.createElement("div");
+testInsert.classList.add("testClass", "penisman");
+let dataId = "penischild";
+let dataName = "test name";
+let dataCost = 50;
+testInsert.setAttribute("data-test", dataId);
+testInsert.innerHTML = `
+<p>${dataName}</p>
+<b>but ${dataCost} fr</b>
+`;
+const tierOneUpgradesSection = document.getElementById("tierOneUpgrades");
+tierOneUpgradesSection.appendChild(testInsert);
+*/
