8acd3d395ac1b14e161da7a7fb908d363ebb1b15
Humble Bumble <s.czarnowski.2008@wp.pl>
2025-10-03 22:34:00 +0200

    optimised fetching and showing elements on the site

--- main.js
+++ main.js
@@ -14,15 +14,16 @@ fetch("database.json")
     .then(data => data.json())
     .then(jsonData => {
         originalData = jsonData;
-        initializeDynamicData();
-        createUpgradeElements();
+        initializeDynamicData("CPS_upgrades");
+        createUpgradeElements("CPS_upgrades", "tierOneUpgrades");
+        createUpgradeElements("CPC_upgrades", "tierTwoUpgrades");
     })
     .catch(error => {
     console.error('Error loading upgrades:', error);
 });
 
-function initializeDynamicData() {
-    originalData.infinite_upgrades.forEach(upgrade => {
+function initializeDynamicData(dataSet) {
+    originalData[dataSet].forEach(upgrade => {
         dynamicData[upgrade.upgrade_id] = {
             upgradeId: upgrade.upgrade_id,
             upgradeQuantity: upgrade.upgrade_quantity,
@@ -31,15 +32,16 @@ function initializeDynamicData() {
     });    
 }
 
-function createUpgradeElements() {
-    originalData.infinite_upgrades.forEach(upgrade => {
+function createUpgradeElements(dataSet, tierId) {
+    originalData[dataSet].forEach(upgrade => {
         let id = upgrade.upgrade_id
         let name = upgrade.upgrade_name;
         let tooltip = upgrade.upgrade_tooltip;
         let quantity = upgrade.upgrade_quantity;
         let cost = upgrade.upgrade_cost;
-        let CPCincrease = upgrade.upgrade_upgrade_increaseCPCby;
-        let CPSincrease = upgrade.upgrade_upgrade_increaseCPSby;
+        let CPCincrease = upgrade.upgrade_increaseCPCby;
+        let CPSincrease = upgrade.upgrade_increaseCPSby;
+        let oneTime = upgrade.upgrade_oneTime;
 
         const attributes = {
             'data-id': id,
@@ -48,7 +50,8 @@ function createUpgradeElements() {
             'data-quantity': quantity,
             'data-cost': cost,
             'data-CPCincrease': CPCincrease,
-            'data-CPSincrease': CPSincrease
+            'data-CPSincrease': CPSincrease,
+            'data-oneTime': oneTime
         }
 
         const templateDiv = document.createElement("div");
@@ -60,42 +63,10 @@ function createUpgradeElements() {
             <p class="nameText">${name}</p>
             <p class="costText">Cost: ${cost}</p>
         `
-        const tierOneUpgradesSection = document.getElementById("tierOneUpgrades");
-        tierOneUpgradesSection.appendChild(templateDiv);
-    });
-    // You might say this is inefficent cause im repeating the exact same code with like 3 things changed, i say to that: go fuck yourself, thanks
-    originalData.finite_upgrades.forEach(upgrade => { 
-        let id = upgrade.upgrade_id
-        let name = upgrade.upgrade_name;
-        let tooltip = upgrade.upgrade_tooltip;
-        let cost = upgrade.upgrade_cost;
-        let CPCincrease = upgrade.upgrade_upgrade_increaseCPCby;
-        let CPSincrease = upgrade.upgrade_upgrade_increaseCPSby;
-
-        const attributes = {
-            'data-id': id,
-            'data-name': name,
-            'data-tooltip': tooltip,
-            'data-cost': cost,
-            'data-CPCincrease': CPCincrease,
-            'data-CPSincrease': CPSincrease
-        }
-
-        const templateDiv = document.createElement("div");
-        templateDiv.classList.add("leftDivChildElement");
-        for (const [key, value] of Object.entries(attributes)) {
-            templateDiv.setAttribute(key, value);
-        }
-        templateDiv.innerHTML = `
-            <p class="nameText">${name}</p>
-            <p class="costText">Cost: ${cost}</p>
-        `
-        const tierTwoUpgradesSection = document.getElementById("tierTwoUpgrades");
-        tierTwoUpgradesSection.appendChild(templateDiv);
+        const tierSection = document.getElementById(tierId);
+        tierSection.appendChild(templateDiv);
     });
 };
-
-
 
 function addPoints() {
     points += pointsToAdd;
